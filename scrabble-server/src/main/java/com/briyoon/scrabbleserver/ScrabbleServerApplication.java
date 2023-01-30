892e87cd13b1c13f613aa85a1ddd77d4b4e6df52
brian <brainmccollum@unm.edu>
2023-01-29 18:10:54 -0700

    project structure init

---
+++ scrabble-server/src/main/java/com/briyoon/scrabbleserver/ScrabbleServerApplication.java
@@ -0,0 +1,13 @@
+package com.briyoon.scrabbleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrabbleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrabbleServerApplication.class, args);
+	}
+
+}
---
+++ scrabble-server/src/test/java/com/briyoon/scrabbleserver/ScrabbleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briyoon.scrabbleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrabbleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
