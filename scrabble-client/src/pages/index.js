8cf1b54978c7b015d67606023a914a9feb4b9056
Brian <bmccollum00@gmail.com>
2023-02-11 08:21:40 +0000

    switched to next because vite bad

---
+++ scrabble-client/src/pages/index.js
@@ -0,0 +1,42 @@
+import { useRouter } from "next/router";
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+        <button className={"menubutton"} onClick={postGame}>Create Game</button>
+        <button className={"menubutton"} disabled={true}>Resume Game</button>
+    </div>
+  )
+}
+
+async function postGame() {
+    console.log("Creating game")
+
+    const router = useRouter();
+
+    const res = await fetch(
+        "/api/games",
+        {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        cache: 'default'
+        }
+    );
+
+    console.log(res);
+
+    if (res.status == 200) {
+        let body = await res.json();
+        console.log(body);
+
+        // Nav to new games page
+        router.push(`/game/${body.gameID}`)
+    }
+}
+
+export default App
